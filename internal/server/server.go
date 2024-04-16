c278e063cd75cc23bf9d3c236da4ea157d817da6
chezzijr <nhuthuan0501@gmail.com>
2024-04-16 10:34:55 +0700

    Initial commit

---
+++ internal/server/server.go
@@ -0,0 +1,26 @@
+package server
+
+import (
+	"github.com/gofiber/fiber/v2"
+
+	"p2p/internal/database"
+)
+
+type FiberServer struct {
+	*fiber.App
+
+	db database.Service
+}
+
+func New() *FiberServer {
+	server := &FiberServer{
+		App: fiber.New(fiber.Config{
+			ServerHeader: "p2p",
+			AppName:      "p2p",
+		}),
+
+		db: database.New(),
+	}
+
+	return server
+}
