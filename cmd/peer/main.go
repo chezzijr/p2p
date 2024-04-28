6221041f7318c54cf04f17c56cbd4b506ad4a20d
chezzijr <nhuthuan0501@gmail.com>
2024-04-28 11:36:36 +0700

    feat(db): add healthcheck and redis

--- cmd/peer/main.go
+++ cmd/peer/main.go
@@ -11,58 +11,71 @@ import (
 )
 
 var (
-    port = flag.Uint("port", 1234, "port to listen on")
-    seedingTorrent = flag.String("seed", "", "torrent file to seed")
-    downloadTorrent = flag.String("download", "", "torrent file to download")
+	port            = flag.Uint("port", 1234, "port to listen on")
+	seedingTorrent  = flag.String("seed", "", "torrent file to seed")
+	downloadTorrent = flag.String("download", "", "torrent file to leech")
 )
 
 func main() {
-    flag.Parse()
+	flag.Parse()
 
-    p, err := peer.NewPeer(uint16(*port))
-    if err != nil {
-        panic(err)
-    }
+	if flag.NArg() == 0 {
+		flag.Usage()
+		return
+	}
 
-    if *seedingTorrent != "" {
-        filename := *seedingTorrent
-        ut, err := torrent.GenerateTorrentFromSingleFile(filename, "http://localhost:8081/announce", 1024)
-        if err != nil {
-            panic(err)
-        }
+	// get first argument
+	cmd := flag.Arg(0)
+	switch cmd {
+	case "search":
+		slog.Info("Searching for torrents")
 
-        err = ut.Save(ut.Name + ".torrent")
-        if err != nil {
-            panic(err)
-        }
+	case "run":
+		p, err := peer.NewPeer(uint16(*port))
+		if err != nil {
+			panic(err)
+		}
 
-        slog.Info("Seeding", "torrent", ut.Name)
-        p.AddTorrent(ut)
-    }
+		if *seedingTorrent != "" {
+			filename := *seedingTorrent
+			ut, err := torrent.GenerateTorrentFromSingleFile(filename, "http://localhost:8081/announce", 1024)
+			if err != nil {
+				panic(err)
+			}
 
-    if *downloadTorrent != "" {
-        dt, err := torrent.Open(*downloadTorrent)
-        if err != nil {
-            panic(err)
-        }
+			err = ut.Save(ut.Name + ".torrent")
+			if err != nil {
+				panic(err)
+			}
 
-        slog.Info("Downloading", "torrent", dt.Name)
-        go func(){
-            err = p.Download(dt, "tests")
-            if err != nil {
-                panic(err)
-            }
-        }()
-    }
+			slog.Info("Seeding", "torrent", ut.Name)
+			p.AddTorrent(ut)
+		}
 
-    slog.Info("Listening", "port", *port)
+		if *downloadTorrent != "" {
+			dt, err := torrent.Open(*downloadTorrent)
+			if err != nil {
+				panic(err)
+			}
 
-    err = p.RunServer()
-    if err != nil {
-        if errors.Is(err, io.EOF) {
-            slog.Info("Connection closed unexpectedly")
-        } else {
-            slog.Error("Error", "error", err)
-        }
-    }
+			slog.Info("Downloading", "torrent", dt.Name)
+			go func() {
+				err = p.Download(dt, "tests")
+				if err != nil {
+					panic(err)
+				}
+			}()
+		}
+
+		slog.Info("Listening", "port", *port)
+
+		err = p.RunServer()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				slog.Info("Connection closed unexpectedly")
+			} else {
+				slog.Error("Error", "error", err)
+			}
+		}
+	}
 }
